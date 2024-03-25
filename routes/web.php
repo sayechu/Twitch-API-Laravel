6ca47dba0a9fdc9e3c146e63dd36d2581d7f8b4d
Santi Ayechu <santiayechu@gmail.com>
2024-03-25 19:12:23 +0100

    [4] - añadir controlador para el endpoint STREAMS

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AnalyticsUsersController;
+use App\Http\Controllers\AnalyticsStreamsController;
 
 Route::get('/', function () {
     return view('welcome');
 });
  
 Route::get('/analytics/users', AnalyticsUsersController::class);
+
+Route::get('/analytics/streams', AnalyticsStreamsController::class);
