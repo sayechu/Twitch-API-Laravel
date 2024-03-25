a80abbedfef0f1c1b7198d7b0ff6e5a40da09bf3
sayechu <santiayechu@gmail.com>
2024-03-25 19:29:11 +0100

    [5] - añadir controlador para el endpoint topsOfTheTops (#8)

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AnalyticsUsersController;
 use App\Http\Controllers\AnalyticsStreamsController;
+use App\Http\Controllers\AnalyticsTopsOfTheTopsController;
 
 Route::get('/', function () {
     return view('welcome');
 });
  
 Route::get('/analytics/users', AnalyticsUsersController::class);
-
 Route::get('/analytics/streams', AnalyticsStreamsController::class);
+Route::get('/analytics/topsofthetops', AnalyticsTopsOfTheTopsController::class);
