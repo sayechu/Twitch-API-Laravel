e7d30701686d1445639d1e845c0a06712807c5a0
Santi Ayechu <santiayechu@gmail.com>
2024-03-26 10:21:17 +0100

    [5] - actualizar controlador endpoint topsOfTheTops para que devuelva en formato JSON

--- app/Http/Controllers/AnalyticsTopsOfTheTopsController.php
+++ app/Http/Controllers/AnalyticsTopsOfTheTopsController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\TwitchApi;
+use App\Services\Database;
+use PDO;
 
 class AnalyticsTopsOfTheTopsController extends Controller
 {
@@ -11,6 +14,74 @@ class AnalyticsTopsOfTheTopsController extends Controller
      */
     public function __invoke(Request $request)
     {
-        include_once __DIR__ . '/../../Services/topsofthetops.php';
+        $client_id = '970almy6xw98ruyojcwqpop0p0o5a2';
+        $client_secret = 'yl0nqzjjnadd8wl7zilpr9pzuh979j';
+
+        $db = new Database();
+        $twitchApi = new TwitchApi($client_id, $client_secret);
+
+        $since = $request->input('since') ?? null;
+        $lastUpdate = $db->obtenerFechaUltimaInsercion();
+        $tenMinutesInSeconds = 10 * 60;
+        $shouldUpdate = false;
+
+        if ($lastUpdate) {
+            $lastUpdateTime = strtotime($lastUpdate['fecha']);
+            $currentTime = time();
+
+            $since = $since ?? $tenMinutesInSeconds;
+
+            if (($currentTime - $lastUpdateTime) > $since) {
+                $shouldUpdate = true;
+            }
+        } else {
+            // Si no hay registros, asumimos que se debe actualizar
+            $shouldUpdate = true;
+        }
+
+        if ($shouldUpdate) {
+            $db->clearTablas();
+
+            $topGamesData = $twitchApi->getTopGames();
+            $db->insertarTopGames($topGamesData);
+
+            foreach ($topGamesData['data'] as $game) {
+                $gameId = $game['id'];
+                $topVideosData = $twitchApi->getTop40VideosDadoUnGameId($gameId);
+                $db->insertarVideos($topVideosData, $gameId);
+            }
+        }
+
+        $stmt = $db->obtenerNombreJuegoEId();
+        $results = [];
+
+        foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $fila) {
+            $gameId = $fila['gameid'];
+            $gameName = $fila['gamename'];
+
+            $stmtAtr = $db->obtenerAtributos($gameId);
+            if ($row = $stmtAtr->fetch(PDO::FETCH_ASSOC)) {
+                $result = [
+                    'game_id' => strval($gameId),
+                    'game_name' => $gameName,
+                    'user_name' => $row['user_name'],
+                    'total_videos' => strval($row['total_videos']),
+                    'total_views' => strval($row['total_views']),
+                    'most_viewed_title' => $row['most_viewed_title'],
+                    'most_viewed_views' => strval($row['most_viewed_views']),
+                    'most_viewed_duration' => $row['most_viewed_duration'],
+                    'most_viewed_created_at' => $row['most_viewed_created_at']
+                ];
+
+                $results[] = $result;
+            } else {
+                $result = [
+                    'error' => 'No se encontraron datos para el juego con ID ' . $gameId
+                ];
+                $results[] = $result;
+            }
+        }
+
+        return response()->json($results, 200, [], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
     }
 }
