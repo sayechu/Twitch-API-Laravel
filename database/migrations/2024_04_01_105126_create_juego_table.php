887d20696fea0d61d72d9b4cd4ef25f9200a419f
Santi Ayechu <santiayechu@gmail.com>
2024-04-01 13:01:00 +0200

    [11] - añadir nuevas migraciones de tablas con creacion correcta y claves foraneas bien

---
+++ database/migrations/2024_04_01_105126_create_juego_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        $sql = "
+            CREATE TABLE JUEGO (
+                position SERIAL,
+                gameId INT PRIMARY KEY,
+                gameName VARCHAR(255),
+                idFecha BIGINT UNSIGNED,
+                CONSTRAINT FK_FECHACONSULTA FOREIGN KEY (idFecha) REFERENCES FECHACONSULTA(idFecha) 
+            )
+        ";
+
+        DB::statement($sql);
+    } 
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::statement("DROP TABLE IF EXISTS JUEGO");
+    }
+};
