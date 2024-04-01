cc0b209f6e0d6b36c9b6b6f0fa07515ed9723ad9
Santi Ayechu <santiayechu@gmail.com>
2024-04-01 13:07:39 +0200

    [2] - quitar comprobación de ID, ya que tenemos de validadores de la/s peticion/es

--- app/Http/Controllers/AnalyticsUsersController.php
+++ app/Http/Controllers/AnalyticsUsersController.php
@@ -10,18 +10,14 @@ class AnalyticsUsersController extends Controller
 {
     public function __invoke(AnalyticsUsersRequest $request)
     {
-        if ($request->has('id')) {
-            $userId = $request->input('id');
+        $userId = $request->input('id');
 
-            $client_id = '970almy6xw98ruyojcwqpop0p0o5a2';
-            $client_secret = 'yl0nqzjjnadd8wl7zilpr9pzuh979j';
+        $client_id = '970almy6xw98ruyojcwqpop0p0o5a2';
+        $client_secret = 'yl0nqzjjnadd8wl7zilpr9pzuh979j';
 
-            $twitchApi = new TwitchApi($client_id, $client_secret);
-            $userInfo = $twitchApi->getInfoUser($userId);
+        $twitchApi = new TwitchApi($client_id, $client_secret);
+        $userInfo = $twitchApi->getInfoUser($userId);
 
-            return response()->json($userInfo);
-        } else {
-            return response()->json(['error' => 'No user ID provided in the request.'], 400);
-        }
+        return response()->json($userInfo, 200, [], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
     }
 }
