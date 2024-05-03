f40edfd46c8f2e07a288a749a61777c882d8075f
Santi Ayechu <santiayechu@gmail.com>
2024-05-03 20:10:39 +0200

    [22] - Añadir test de integración endpoint streams usando dobles para dependencia

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,18 +2,46 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use Mockery;
 
 class ExampleTest extends TestCase
 {
     /**
-     * A basic test example.
+     * @test
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function gets_streams()
     {
-        $response = $this->get('/');
+        $apiClient = Mockery::mock(ApiClient::class);
+        $this->app
+            ->when(StreamsManager::class)
+            ->needs(ApiClient::class)
+            ->give(fn() => $apiClient);
+        $getTokenExpectedResponse = json_encode([
+            'access_token' => 'zfmr6i7cbwken2maslfu9v89tvq9ne',
+            'expires_in' => 5443987,
+            'token_type' => 'bearer'
+        ]);
+        $getStreamsExpectedResponse = json_encode(['data' => [[
+            'title' => 'Stream title',
+            'user_name' => 'user_name'
+        ]]]);
+
+        $apiClient
+            ->expects('getToken')
+            ->with('https://id.twitch.tv/oauth2/token')
+            ->once()
+            ->andReturn($getTokenExpectedResponse);
+        $apiClient
+            ->expects('makeCurlCall')
+            ->with('https://id.twitch.tv/helix/streams', [0 => 'Authorization: Bearer zfmr6i7cbwken2maslfu9v89tvq9ne'])
+            ->once()
+            ->andReturn($getStreamsExpectedResponse);
+
+        $response = $this->get('/analytics/streams');
 
         $response->assertStatus(200);
+        $response->assertContent('[{"title":"Stream title", "user_name":"user_name"}]');
     }
+
 }
