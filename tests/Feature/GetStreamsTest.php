ec43924d390d046ffb46192812a720afbc5b003e
sayechu <santiayechu@gmail.com>
2024-05-04 13:27:48 +0200

    [22] - Refactor endpoint streams (#25)
    * [22] - Separar endpoint en más componentes -> Controlador + Servicio + Manager
    
    * [22] - Añadir test de integración endpoint streams usando dobles para dependencia
    
    * [22] - Renombrar metodo getStreamsCall a makeCurlCall para reutilizarlo
    
    * [22] - Actualizar test de integración get_streams()
    
    * [22] - Renombrar clase de test de ExampleTest a GetStreamsTest
    
    * [22] - Renombrar clase de test de ExampleTest a GetStreamsTest
    
    * [22] - Añadir tipo de return value: void a Test get_streams()
    
    * [22] - Crear Tests Unitario de GetStreamsService
    
    * [22] - Borrar importacion innecesaria
    
    * [22] - Borrar atributo duplicado user_name en expectedStreams
    
    ---------
    
    Co-authored-by: eduezponda <eduezponda@gmail.com>

---
+++ tests/Feature/GetStreamsTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Services\ApiClient;
+use App\Services\StreamsManager;
+use Tests\TestCase;
+use Mockery;
+
+class GetStreamsTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function gets_streams(): void
+    {
+        $apiClient = Mockery::mock(ApiClient::class);
+        $this->app
+            ->when(StreamsManager::class)
+            ->needs(ApiClient::class)
+            ->give(fn() => $apiClient);
+        $getTokenExpectedResponse = json_encode([
+            'access_token' => 'zfmr6i7cbwken2maslfu9v89tvq9ne',
+            'expires_in' => 5443987,
+            'token_type' => 'bearer'
+        ]);
+        $getStreamsExpectedResponse = json_encode(['data' => [[
+            'title' => 'Stream title',
+            'user_name' => 'user_name'
+        ]]]);
+
+        $apiClient
+            ->expects('getToken')
+            ->once()
+            ->andReturn($getTokenExpectedResponse);
+        $twitchToken = json_decode($getTokenExpectedResponse, true)['access_token'];
+        $apiClient
+            ->expects('makeCurlCall')
+            ->with("https://api.twitch.tv/helix/streams", [0 => 'Authorization: Bearer ' . $twitchToken])
+            ->once()
+            ->andReturn($getStreamsExpectedResponse);
+
+        $response = $this->get('/analytics/streams');
+
+        $response->assertStatus(200);
+        $response->assertContent('[{"title":"Stream title","user_name":"user_name"}]');
+    }
+}
