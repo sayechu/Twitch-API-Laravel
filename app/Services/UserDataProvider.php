d1be9edea58261e9060d38a6953e617056cb0bb4
Santi Ayechu <santiayechu@gmail.com>
2024-05-06 17:56:33 +0200

    [29] - Implementar método getUserData() de la clase UserDataProvider

---
+++ app/Services/UserDataProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Services;
+
+class UserDataProvider
+{
+    private ApiClient $apiClient;
+
+    public function __construct(ApiClient $apiClient)
+    {
+        $this->apiClient = $apiClient;
+    }
+
+    public function getUserData(string $api_url, array $api_headers): string
+    {
+        $resultCurlCall = $this->apiClient->makeCurlCall($api_url, $api_headers);
+
+        if ($this->isA500Code($resultCurlCall['http_code'])) {
+            return '503: {"error": "No se pueden devolver usuarios en este momento,
+            inténtalo más tarde"}';
+        }
+
+        return $resultCurlCall['response'];
+    }
+
+    private function isA500Code(int $http_code): bool
+    {
+        return $http_code == 500;
+    }
+}
