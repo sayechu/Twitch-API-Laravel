c700186b867e5ee7565ad11c5577d6af7274dfa6
Santi Ayechu <santiayechu@gmail.com>
2024-05-08 01:09:02 +0200

    [28] - Actualizar nombre de Servicio a StreamsDataManager

--- app/Http/Controllers/AnalyticsStreamsController.php
+++ app/Http/Controllers/AnalyticsStreamsController.php
@@ -2,21 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use App\Services\GetStreamsService;
+use App\Services\StreamsDataManager;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class AnalyticsStreamsController extends Controller
 {
-    private GetStreamsService $getStreamsService;
-    public function __construct(GetStreamsService $getStreamsService)
+    private StreamsDataManager $streamsDataManager;
+    public function __construct(StreamsDataManager $streamsDataManager)
     {
-        $this->getStreamsService = $getStreamsService;
+        $this->streamsDataManager = $streamsDataManager;
     }
 
     public function __invoke(Request $request): JsonResponse
     {
-        $streams = $this->getStreamsService->getStreams();
+        $streams = $this->streamsDataManager->getStreamsData();
 
         return response()->json($streams);
     }
