975bbfaf6b6d501cda08192e45d92dbb897b94ff
Santi Ayechu <santiayechu@gmail.com>
2024-05-08 17:02:46 +0200

    [28] - Eliminar la capa de StreamsDataProvider y manejar logica desde Manager

--- app/Services/StreamsDataManager.php
+++ app/Services/StreamsDataManager.php
@@ -2,21 +2,35 @@
 
 namespace App\Services;
 
+use Illuminate\Http\Response;
+
 class StreamsDataManager
 {
     private TokenProvider $tokenProvider;
-    private StreamsDataProvider $streamsDataProvider;
+    private ApiClient $apiClient;
 
-    public function __construct(TokenProvider $tokenProvider, StreamsDataProvider $streamsDataProvider)
+    public function __construct(TokenProvider $tokenProvider, ApiClient $apiClient)
     {
         $this->tokenProvider = $tokenProvider;
-        $this->streamsDataProvider = $streamsDataProvider;
+        $this->apiClient = $apiClient;
     }
 
     public function getStreamsData(): array
     {
+        $api_url = 'https://api.twitch.tv/helix/streams';
         $api_headers = array('Authorization: Bearer ' . $this->tokenProvider->getToken());
 
-        return $this->streamsDataProvider->streamsUserData($api_headers);
+        $responseCurlCall = $this->apiClient->makeCurlCall($api_url, $api_headers);
+
+        if ($this->isA500Code($responseCurlCall['http_code'])) {
+            return '503: {"error": "No se pueden devolver streams en este momento, inténtalo más tarde"}';
+        }
+
+        return json_decode($responseCurlCall['response'], true)['data'];
+    }
+
+    private function isA500Code(int $http_code): bool
+    {
+        return $http_code === Response::HTTP_INTERNAL_SERVER_ERROR;
     }
 }
