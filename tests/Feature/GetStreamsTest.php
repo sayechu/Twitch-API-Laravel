8d7803225785c3cbc94047a546f3cbb66cfb9d76
Santi Ayechu <santiayechu@gmail.com>
2024-05-08 16:00:53 +0200

    [28] - Actualizar test de integración

--- tests/Feature/GetStreamsTest.php
+++ tests/Feature/GetStreamsTest.php
@@ -3,7 +3,9 @@
 namespace Tests\Feature;
 
 use App\Services\ApiClient;
-use App\Services\StreamsManager;
+use App\Services\DBClient;
+use App\Services\StreamsDataProvider;
+use App\Services\TokenProvider;
 use Tests\TestCase;
 use Mockery;
 
@@ -12,38 +14,65 @@ class GetStreamsTest extends TestCase
     /**
      * @test
      */
-    public function gets_streams(): void
+    public function gets_streams_with_token_stored_in_database(): void
     {
         $apiClient = Mockery::mock(ApiClient::class);
+        $databaseClient = Mockery::mock(DBClient::class);
         $this->app
-            ->when(StreamsManager::class)
+            ->when(TokenProvider::class)
             ->needs(ApiClient::class)
             ->give(fn() => $apiClient);
-        $getTokenExpectedResponse = json_encode([
-            'access_token' => 'zfmr6i7cbwken2maslfu9v89tvq9ne',
-            'expires_in' => 5443987,
-            'token_type' => 'bearer'
-        ]);
-        $getStreamsExpectedResponse = json_encode(['data' => [[
-            'title' => 'Stream title',
-            'user_name' => 'user_name'
-        ]]]);
+        $this->app
+            ->when(TokenProvider::class)
+            ->needs(DBClient::class)
+            ->give(fn() => $databaseClient);
+        $this->app
+            ->when(StreamsDataProvider::class)
+            ->needs(ApiClient::class)
+            ->give(fn() => $apiClient);
 
-        $apiClient
+        $getStreamsExpectedResponse = [
+            'response' => json_encode([
+                'data' => [
+                    [
+                        'id' => '40627613557',
+                        'user_id' => '92038375',
+                        'user_login' => 'caedrel',
+                        'user_name' => 'User Name',
+                        'game_id' => '21779',
+                        'game_name' => 'League of Legends',
+                        'type' => 'live',
+                        'title' => 'Stream Title',
+                        'viewer_count' => 46181,
+                        'started_at' => '2024-05-08T07:35:07Z',
+                        'language' => 'en',
+                        'thumbnail_url' => 'https://static-cdn.jtvnw.net/previews-ttv/live_user_caedrel-{width}x{height}.jpg',
+                        'tag_ids' => [],
+                        'tags' => ['xdd', 'Washed', 'degen', 'English', 'adhd', 'vtuber', 'Ratking', 'LPL', 'LCK', 'LEC'],
+                        'is_mature' => false
+                    ]
+                ]
+            ]),
+            'http_code' => 200
+        ];
+        $databaseClient
+            ->expects('isTokenStoredInDatabase')
+            ->once()
+            ->andReturn(true);
+        $databaseClient
             ->expects('getToken')
             ->once()
-            ->andReturn($getTokenExpectedResponse);
-        $twitchToken = json_decode($getTokenExpectedResponse, true)['access_token'];
+            ->andReturn('zfmr6i7cbwken2maslfu9v89tvq9ne');
         $apiClient
             ->expects('makeCurlCall')
-            ->with("https://api.twitch.tv/helix/streams", [0 => 'Authorization: Bearer ' . $twitchToken])
+            ->with("https://api.twitch.tv/helix/streams", [0 => 'Authorization: Bearer zfmr6i7cbwken2maslfu9v89tvq9ne'])
             ->once()
             ->andReturn($getStreamsExpectedResponse);
 
         $response = $this->get('/analytics/streams');
 
         $response->assertStatus(200);
-        $response->assertContent('[{"title":"Stream title","user_name":"user_name"}]');
+        $response->assertContent('[{"title":"Stream Title","user_name":"User Name"}]');
     }
 
     protected function tearDown(): void
