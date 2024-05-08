14fdb9912e60851dbc2a9c5bd660eda76cbfdccc
Santi Ayechu <santiayechu@gmail.com>
2024-05-08 23:15:37 +0200

    [29] - Añadir carpeta tests/Feature para pasar test

---
+++ tests/Feature/Test.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature;
+
+use PHPUnit\Framework\TestCase;
+
+class Test extends TestCase
+{
+    /**
+     * @test
+     */
+    public function test(): void
+    {
+        $this->assertEquals('', '');
+    }
+
+}
