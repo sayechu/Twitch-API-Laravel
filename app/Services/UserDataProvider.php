97c8fa81a019413e96cecac3919e6cf87e804558
Santi Ayechu <santiayechu@gmail.com>
2024-05-09 10:56:31 +0200

    [29] - Eliminar lógica del UserDataProvider

--- app/Services/UserDataProvider.php
+++ app/Services/UserDataProvider.php
@@ -13,18 +13,6 @@ class UserDataProvider
 
     public function getUserData(string $api_url, array $api_headers): array
     {
-        $resultCurlCall = $this->apiClient->makeCurlCall($api_url, $api_headers);
-
-        if ($this->isA500Code($resultCurlCall['http_code'])) {
-            return '503: {"error": "No se pueden devolver usuarios en este momento,
-            inténtalo más tarde"}';
-        }
-
-        return json_decode($resultCurlCall['response'], true);
-    }
-
-    private function isA500Code(int $http_code): bool
-    {
-        return $http_code == 500;
+        return $this->apiClient->makeCurlCall($api_url, $api_headers);
     }
 }
