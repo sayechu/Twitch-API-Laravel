80a40d1c33c39869589b342b2d2c863c98509927
Santi Ayechu <santiayechu@gmail.com>
2024-05-09 10:51:52 +0200

    [29] - Añadir tests de validación de Form Request

--- tests/Unit/Infrastructure/Requests/AnalyticsUsersRequestTest.php
+++ tests/Unit/Infrastructure/Requests/AnalyticsUsersRequestTest.php
@@ -2,7 +2,7 @@
 
 namespace Tests\Unit\Infrastructure\Requests;
 
-use App\Http\Requests\AnalyticsUsersRequest;
+use App\Infrastructure\Requests\AnalyticsUsersRequest;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\MessageBag;
 use Tests\TestCase;
