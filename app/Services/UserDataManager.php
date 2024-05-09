5535132c14c2505bad030e1eae6272b4240cc6ed
Santi Ayechu <santiayechu@gmail.com>
2024-05-09 10:52:36 +0200

    [29] - Añadir lógica de UserDataProvider en UserDataManager

--- app/Services/UserDataManager.php
+++ app/Services/UserDataManager.php
@@ -2,6 +2,8 @@
 
 namespace App\Services;
 
+use Illuminate\Http\Response;
+
 class UserDataManager
 {
     private TokenProvider $tokenProvider;
@@ -16,10 +18,20 @@ class UserDataManager
     public function getUserData(string $userId): array
     {
         $api_url = "https://api.twitch.tv/helix/users?id=" . urlencode($userId);
+        $api_headers = array('Authorization: Bearer ' . $this->tokenProvider->getToken());
 
-        $twitchToken = $this->tokenProvider->getToken();
+        $userData = $this->userDataProvider->getUserData($api_url, $api_headers);
 
-        $api_headers = array('Authorization: Bearer ' . $twitchToken);
-        return $this->userDataProvider->getUserData($api_url, $api_headers);
+        if ($this->isA500Code($userData['http_code'])) {
+            return '503: {"error": "No se pueden devolver usuarios en este momento,
+            inténtalo más tarde"}';
+        }
+
+        return json_decode($userData['response'], true);
+    }
+
+    private function isA500Code(int $http_code): bool
+    {
+        return $http_code === Response::HTTP_INTERNAL_SERVER_ERROR;
     }
 }
