ddb3e20ed2d2a21a2132bffab32036d4cadec480
Santi Ayechu <santiayechu@gmail.com>
2024-05-10 01:33:39 +0200

    [29] - Refactorizar tests + añadir test de integración

--- tests/Feature/GetUsersTest.php
+++ tests/Feature/GetUsersTest.php
@@ -36,9 +36,9 @@ class GetUsersTest extends TestCase
     /**
      * @test
      */
-    public function get_users(): void
+    public function test_get_users_with_token_stored_returns_user_data(): void
     {
-        $expectedUsersResponse = [
+        $usersResponse = [
             'response' => json_encode([
                 'data' => [
                     [
@@ -69,7 +69,62 @@ class GetUsersTest extends TestCase
             ->expects('makeCurlCall')
             ->with("https://api.twitch.tv/helix/users?id=1234", [0 => 'Authorization: Bearer nrtovbe5h02os45krmjzvkt3hp74vf'])
             ->once()
-            ->andReturn($expectedUsersResponse);
+            ->andReturn($usersResponse);
+
+        $responseGetUsers = $this->get('/analytics/users?id=1234');
+
+        $responseGetUsers->assertContent('{"data":[{"id":"userId","login":"userLogin","display_name":"displayName","type":"type","broadcaster_type":"broadcasterType","description":"description","profile_image_url":"profileImageUrl","offline_image_url":"offlineImageUrl","view_count":0,"created_at":"createdAt"}]}');
+    }
+
+    /**
+     * @test
+     */
+    public function test_get_users_with_token_from_api_returns_user_data()
+    {
+        $userResponse = [
+            'response' => json_encode([
+                'data' => [
+                    [
+                        'id' => 'userId',
+                        'login' => 'userLogin',
+                        'display_name' => 'displayName',
+                        'type' => 'type',
+                        'broadcaster_type' => 'broadcasterType',
+                        'description' => 'description',
+                        'profile_image_url' => 'profileImageUrl',
+                        'offline_image_url' => 'offlineImageUrl',
+                        'view_count' => 0,
+                        'created_at' => 'createdAt'
+                    ]
+                ]
+            ]),
+            'http_code' => 200
+        ];
+        $getTokenResponse = [
+            'response' => json_encode([
+                'access_token' => 'rz2po6wrbgami5k2qjk0e4q0vwschm',
+                'expires_in' => 5089418,
+                'token_type' => 'bearer'
+            ]),
+            'http_code' => 200
+        ];
+        $this->databaseClient
+            ->expects('isTokenStoredInDatabase')
+            ->once()
+            ->andReturn(false);
+        $this->apiClient
+            ->expects('getToken')
+            ->once()
+            ->andReturn($getTokenResponse);
+        $this->databaseClient
+            ->expects('storeToken')
+            ->once()
+            ->with('rz2po6wrbgami5k2qjk0e4q0vwschm');
+        $this->apiClient
+            ->expects('makeCurlCall')
+            ->with("https://api.twitch.tv/helix/users?id=1234", [0 => 'Authorization: Bearer rz2po6wrbgami5k2qjk0e4q0vwschm'])
+            ->once()
+            ->andReturn($userResponse);
 
         $responseGetUsers = $this->get('/analytics/users?id=1234');
 
