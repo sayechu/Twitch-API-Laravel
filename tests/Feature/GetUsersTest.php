36716674b09d2db54b231ad2d9427b06c9464260
Santi Ayechu <santiayechu@gmail.com>
2024-05-10 00:11:38 +0200

    [29] - Actualizar test de integración endpoint Users:Id quitando UsersDataProvider

--- tests/Feature/GetUsersTest.php
+++ tests/Feature/GetUsersTest.php
@@ -2,35 +2,42 @@
 
 namespace Tests\Feature;
 
+use App\Services\UserDataManager;
 use Illuminate\Foundation\Testing\TestCase;
 use App\Services\ApiClient;
 use App\Services\DBClient;
 use App\Services\TokenProvider;
-use App\Services\UserDataProvider;
 use Mockery;
 
 class GetUsersTest extends TestCase
 {
+    private ApiClient $apiClient;
+    private DBClient $databaseClient;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->apiClient = Mockery::mock(ApiClient::class);
+        $this->databaseClient = Mockery::mock(DBClient::class);
+        $this->app
+            ->when(TokenProvider::class)
+            ->needs(ApiClient::class)
+            ->give(fn() => $this->apiClient);
+        $this->app
+            ->when(TokenProvider::class)
+            ->needs(DBClient::class)
+            ->give(fn() => $this->databaseClient);
+        $this->app
+            ->when(UserDataManager::class)
+            ->needs(ApiClient::class)
+            ->give(fn() => $this->apiClient);
+    }
+
     /**
      * @test
      */
     public function get_users(): void
     {
-        $apiClient = Mockery::mock(ApiClient::class);
-        $databaseClient = Mockery::mock(DBClient::class);
-        $this->app
-            ->when(TokenProvider::class)
-            ->needs(ApiClient::class)
-            ->give(fn() => $apiClient);
-        $this->app
-            ->when(TokenProvider::class)
-            ->needs(DBClient::class)
-            ->give(fn() => $databaseClient);
-        $this->app
-            ->when(UserDataProvider::class)
-            ->needs(ApiClient::class)
-            ->give(fn() => $apiClient);
-
         $expectedUsersResponse = [
             'response' => json_encode([
                 'data' => [
@@ -50,15 +57,15 @@ class GetUsersTest extends TestCase
             ]),
             'http_code' => 200
         ];
-        $databaseClient
+        $this->databaseClient
             ->expects('isTokenStoredInDatabase')
             ->once()
             ->andReturn(true);
-        $databaseClient
+        $this->databaseClient
             ->expects('getToken')
             ->once()
             ->andReturn('nrtovbe5h02os45krmjzvkt3hp74vf');
-        $apiClient
+        $this->apiClient
             ->expects('makeCurlCall')
             ->with("https://api.twitch.tv/helix/users?id=1234", [0 => 'Authorization: Bearer nrtovbe5h02os45krmjzvkt3hp74vf'])
             ->once()
@@ -67,5 +74,11 @@ class GetUsersTest extends TestCase
         $responseGetUsers = $this->get('/analytics/users?id=1234');
 
         $responseGetUsers->assertContent('{"data":[{"id":"userId","login":"userLogin","display_name":"displayName","type":"type","broadcaster_type":"broadcasterType","description":"description","profile_image_url":"profileImageUrl","offline_image_url":"offlineImageUrl","view_count":0,"created_at":"createdAt"}]}');
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+        Mockery::close();
     }
 }
