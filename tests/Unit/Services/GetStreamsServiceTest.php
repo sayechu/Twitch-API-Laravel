ec2d562c037e713931d8a4d732a2f345a03c7cf5
Santi Ayechu <santiayechu@gmail.com>
2024-05-11 01:21:45 +0200

    [28] - Ajustar nombre de los metodos de test y corregir tests tras cambio de logica

--- tests/Unit/Services/GetStreamsServiceTest.php
+++ tests/Unit/Services/GetStreamsServiceTest.php
@@ -15,136 +15,50 @@ class GetStreamsServiceTest extends TestCase
     protected function setUp(): void
     {
         parent::setUp();
-
         $this->streamsDataManager = Mockery::mock(StreamsDataManager::class);
-
         $this->getStreamsService = new GetStreamsService($this->streamsDataManager);
     }
 
     /**
      * @test
      */
-    public function execute_token_failure(): void
+    public function test_execute(): void
     {
-        $streamsExpectedResponse = '503: {"error": "No se puede establecer conexión con Twitch en este momento}';
-
-        $this->streamsDataManager
-            ->expects('getStreamsData')
-            ->once()
-            ->andReturn($streamsExpectedResponse);
-
-        $returnedStreams = $this->getStreamsService->execute();
-
-        $this->assertEquals('503: {"error": "No se puede establecer conexión con Twitch en este momento}', $returnedStreams);
-    }
-
-    /**
-     * @test
-     */
-    public function execute_token_correct_curl_failure(): void
-    {
-        $streamsExpectedResponse = [
-            'response' => json_encode([
-                'data' => [
-                    [
-                        'id' => '40627613557',
-                        'user_id' => '92038375',
-                        'user_login' => 'caedrel',
-                        'user_name' => 'User Name 1',
-                        'game_id' => '21779',
-                        'game_name' => 'League of Legends',
-                        'type' => 'live',
-                        'title' => 'Stream Title 1',
-                        'viewer_count' => 46181,
-                        'started_at' => '2024-05-08T07:35:07Z',
-                        'language' => 'en',
-                        'thumbnail_url' => 'https://static-cdn.jtvnw.net/previews-ttv/live_user_caedrel-{width}x{height}.jpg',
-                        'tag_ids' => [],
-                        'tags' => ['xdd', 'Washed', 'degen', 'English', 'adhd', 'vtuber', 'Ratking', 'LPL', 'LCK', 'LEC'],
-                        'is_mature' => false
-                    ],
-                    [
-                        'id' => '40627613557',
-                        'user_id' => '92038375',
-                        'user_login' => 'caedrel',
-                        'user_name' => 'User Name 2',
-                        'game_id' => '21779',
-                        'game_name' => 'League of Legends',
-                        'type' => 'live',
-                        'title' => 'Stream Title 2',
-                        'viewer_count' => 46181,
-                        'started_at' => '2024-05-08T07:35:07Z',
-                        'language' => 'en',
-                        'thumbnail_url' => 'https://static-cdn.jtvnw.net/previews-ttv/live_user_caedrel-{width}x{height}.jpg',
-                        'tag_ids' => [],
-                        'tags' => ['xdd', 'Washed', 'degen', 'English', 'adhd', 'vtuber', 'Ratking', 'LPL', 'LCK', 'LEC'],
-                        'is_mature' => false
-                    ]
-                ]
-            ]),
-            'http_code' => 500
-        ];
-
-        $this->streamsDataManager
-            ->expects('getStreamsData')
-            ->once()
-            ->andReturn($streamsExpectedResponse);
-
-        $returnedStreams = $this->getStreamsService->execute();
-
-        $this->assertEquals('503: {"error": "No se pueden devolver streams en este momento, inténtalo más tarde"}', $returnedStreams);
-    }
-
-    /**
-     * @test
-     */
-    public function execute_test(): void
-    {
-        $this->app
-            ->when(GetStreamsService::class)
-            ->needs(StreamsDataManager::class)
-            ->give(fn() => $this->streamsDataManager);
-
-        $streamsExpectedResponse = [
-            'response' => json_encode([
-                'data' => [
-                    [
-                        'id' => '40627613557',
-                        'user_id' => '92038375',
-                        'user_login' => 'caedrel',
-                        'user_name' => 'User Name 1',
-                        'game_id' => '21779',
-                        'game_name' => 'League of Legends',
-                        'type' => 'live',
-                        'title' => 'Stream Title 1',
-                        'viewer_count' => 46181,
-                        'started_at' => '2024-05-08T07:35:07Z',
-                        'language' => 'en',
-                        'thumbnail_url' => 'https://static-cdn.jtvnw.net/previews-ttv/live_user_caedrel-{width}x{height}.jpg',
-                        'tag_ids' => [],
-                        'tags' => ['xdd', 'Washed', 'degen', 'English', 'adhd', 'vtuber', 'Ratking', 'LPL', 'LCK', 'LEC'],
-                        'is_mature' => false
-                    ],
-                    [
-                        'id' => '40627613557',
-                        'user_id' => '92038375',
-                        'user_login' => 'caedrel',
-                        'user_name' => 'User Name 2',
-                        'game_id' => '21779',
-                        'game_name' => 'League of Legends',
-                        'type' => 'live',
-                        'title' => 'Stream Title 2',
-                        'viewer_count' => 46181,
-                        'started_at' => '2024-05-08T07:35:07Z',
-                        'language' => 'en',
-                        'thumbnail_url' => 'https://static-cdn.jtvnw.net/previews-ttv/live_user_caedrel-{width}x{height}.jpg',
-                        'tag_ids' => [],
-                        'tags' => ['xdd', 'Washed', 'degen', 'English', 'adhd', 'vtuber', 'Ratking', 'LPL', 'LCK', 'LEC'],
-                        'is_mature' => false
-                    ]
-                ]
-            ]),
-            'http_code' => 200
+        $streamsResponse = [
+            [
+                'id' => '40627613557',
+                'user_id' => '92038375',
+                'user_login' => 'caedrel',
+                'user_name' => 'User Name 1',
+                'game_id' => '21779',
+                'game_name' => 'League of Legends',
+                'type' => 'live',
+                'title' => 'Stream Title 1',
+                'viewer_count' => 46181,
+                'started_at' => '2024-05-08T07:35:07Z',
+                'language' => 'en',
+                'thumbnail_url' => 'https://static-cdn.jtvnw.net/previews-ttv/live_user_caedrel-{width}x{height}.jpg',
+                'tag_ids' => [],
+                'tags' => ['xdd', 'Washed', 'degen', 'English', 'adhd', 'vtuber', 'Ratking', 'LPL', 'LCK', 'LEC'],
+                'is_mature' => false
+            ],
+            [
+                'id' => '40627613557',
+                'user_id' => '92038375',
+                'user_login' => 'caedrel',
+                'user_name' => 'User Name 2',
+                'game_id' => '21779',
+                'game_name' => 'League of Legends',
+                'type' => 'live',
+                'title' => 'Stream Title 2',
+                'viewer_count' => 46181,
+                'started_at' => '2024-05-08T07:35:07Z',
+                'language' => 'en',
+                'thumbnail_url' => 'https://static-cdn.jtvnw.net/previews-ttv/live_user_caedrel-{width}x{height}.jpg',
+                'tag_ids' => [],
+                'tags' => ['xdd', 'Washed', 'degen', 'English', 'adhd', 'vtuber', 'Ratking', 'LPL', 'LCK', 'LEC'],
+                'is_mature' => false
+            ]
         ];
         $expectedFilteredStreams = [
             [
@@ -160,11 +74,45 @@ class GetStreamsServiceTest extends TestCase
         $this->streamsDataManager
             ->expects('getStreamsData')
             ->once()
-            ->andReturn($streamsExpectedResponse);
+            ->andReturn($streamsResponse);
 
         $returnedStreams = $this->getStreamsService->execute();
 
         $this->assertEquals($expectedFilteredStreams, $returnedStreams);
+    }
+
+    /**
+     * @test
+     */
+    public function test_execute_with_token_error(): void
+    {
+        $streamsResponse = '503: {"error": "No se puede establecer conexión con Twitch en este momento}';
+
+        $this->streamsDataManager
+            ->expects('getStreamsData')
+            ->once()
+            ->andReturn($streamsResponse);
+
+        $returnedStreams = $this->getStreamsService->execute();
+
+        $this->assertEquals($streamsResponse, $returnedStreams);
+    }
+
+    /**
+     * @test
+     */
+    public function execute_token_correct_curl_error(): void
+    {
+        $streamsResponse = '503: {"error": "No se pueden devolver streams en este momento, inténtalo más tarde"}';
+
+        $this->streamsDataManager
+            ->expects('getStreamsData')
+            ->once()
+            ->andReturn($streamsResponse);
+
+        $returnedStreams = $this->getStreamsService->execute();
+
+        $this->assertEquals($streamsResponse, $returnedStreams);
     }
 
     protected function tearDown(): void
