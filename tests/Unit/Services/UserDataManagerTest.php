913bc194ca6312de906cff1cbcea8ccbcaa410bf
Santi Ayechu <santiayechu@gmail.com>
2024-05-11 13:57:52 +0200

    [29] - Recuperar tests UserDataManager y sacar constantes

--- tests/Unit/Services/UserDataManagerTest.php
+++ tests/Unit/Services/UserDataManagerTest.php
@@ -2,56 +2,165 @@
 
 namespace Tests\Unit\Services;
 
-use App\Infrastructure\Requests\AnalyticsUsersRequest;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\MessageBag;
+use App\Services\ApiClient;
+use App\Services\TokenProvider;
+use App\Services\UserDataManager;
+use Mockery;
 use Tests\TestCase;
 
 class UserDataManagerTest extends TestCase
 {
-    /**
-     * @test
-     */
-    public function requestDataIsInvalid(): void
+    private TokenProvider $tokenProvider;
+    private ApiClient $apiClient;
+    private UserDataManager $userDataManager;
+
+    private const ERROR_GET_TOKEN_FAILED = 'No se puede establecer conexión con Twitch en este momento';
+    private const ERROR_GET_USERS_FAILED = 'No se pueden devolver usuarios en este momento, inténtalo más tarde';
+
+    protected function setUp(): void
     {
-        $analyticsUsersRequest = new AnalyticsUsersRequest();
-        $analyticsUsersRequest->merge(['id' => 'abc']);
-        $expectedErrors = new MessageBag([
-            'id' => ['El Id dado no es válido'],
-        ]);
+        parent::setUp();
+        $this->tokenProvider = Mockery::mock(TokenProvider::class);
+        $this->apiClient = Mockery::mock(ApiClient::class);
+        $this->userDataManager = new UserDataManager($this->tokenProvider, $this->apiClient);
 
-        $validationErrors = Validator::make($analyticsUsersRequest->all(), $analyticsUsersRequest->rules(), $analyticsUsersRequest->messages());
-
-        $this->assertTrue($validationErrors->fails());
-        $this->assertEquals($expectedErrors, $validationErrors->errors());
+        $this->app
+            ->when(UserDataManager::class)
+            ->needs(TokenProvider::class)
+            ->give(fn() => $this->tokenProvider);
+        $this->app
+            ->when(UserDataManager::class)
+            ->needs(ApiClient::class)
+            ->give(fn() => $this->apiClient);
     }
 
     /**
      * @test
      */
-    public function idRequiredInRequest(): void
+    public function get_user_data(): void
     {
-        $analyticsUsersRequest = new AnalyticsUsersRequest();
-        $expectedErrors = new MessageBag([
-            'id' => ['El Id es obligatorio'],
-        ]);
+        $getTokenResponse = 'nrtovbe5h02os45krmjzvkt3hp74vf';
+        $getUserDataResponse = [
+            'response' => json_encode([
+                'data' => [
+                    [
+                        'id' => '1234',
+                        'login' => 'zdraste_vladkenov',
+                        'display_name' => 'zdraste_vladkenov',
+                        'type' => '',
+                        'broadcaster_type' => '',
+                        'description' => 'wasde876',
+                        'profile_image_url' => 'https://static-cdn.jtvnw.net/user-default-pictures-uv/ebe4cd89-b4f4-4cd9-adac-2f30151b4209-profile_image-300x300.png',
+                        'offline_image_url' => '',
+                        'view_count' => 0,
+                        'created_at' => '2018-09-04T15:23:04Z'
+                    ]
+                ]
+            ]),
+            'http_code' => 200
+        ];
+        $expectedGetUserDataResponse = [
+            "data" => [
+                [
+                    "id" => "1234",
+                    "login" => "zdraste_vladkenov",
+                    "display_name" => "zdraste_vladkenov",
+                    "type" => "",
+                    "broadcaster_type" => "",
+                    "description" => "wasde876",
+                    "profile_image_url" => "https://static-cdn.jtvnw.net/user-default-pictures-uv/ebe4cd89-b4f4-4cd9-adac-2f30151b4209-profile_image-300x300.png",
+                    "offline_image_url" => "",
+                    "view_count" => 0,
+                    "created_at" => "2018-09-04T15:23:04Z"
+                ]
+            ]
+        ];
 
-        $validationErrors = Validator::make($analyticsUsersRequest->all(), $analyticsUsersRequest->rules(), $analyticsUsersRequest->messages());
+        $this->tokenProvider
+            ->expects('getToken')
+            ->once()
+            ->andReturn($getTokenResponse);
+        $this->apiClient
+            ->expects('makeCurlCall')
+            ->with('https://api.twitch.tv/helix/users?id=1234', [0 => 'Authorization: Bearer nrtovbe5h02os45krmjzvkt3hp74vf'])
+            ->once()
+            ->andReturn($getUserDataResponse);
 
-        $this->assertTrue($validationErrors->fails());
-        $this->assertEquals($expectedErrors, $validationErrors->errors());
+        $returnedUserInfo = $this->userDataManager->getUserData('1234');
+
+        $this->assertEquals($expectedGetUserDataResponse, $returnedUserInfo);
     }
 
     /**
      * @test
      */
-    public function idIsValid(): void
+    public function get_user_data_token_failure(): void
     {
-        $analyticsUsersRequest = new AnalyticsUsersRequest();
-        $analyticsUsersRequest->merge(['id' => 1234]);
+        $getTokenResponse = [
+            'response' => json_encode([
+                'access_token' => 'rz2po6wrbgami5k2qjk0e4q0vwschm',
+                'expires_in' => 5089418,
+                'token_type' => 'bearer'
+            ]),
+            'http_code' => 500
+        ];
+        $expectedResponse = ['error' => self::ERROR_GET_TOKEN_FAILED];
 
-        $validationErrors = Validator::make($analyticsUsersRequest->all(), $analyticsUsersRequest->rules(), $analyticsUsersRequest->messages());
+        $this->tokenProvider
+            ->expects('getToken')
+            ->once()
+            ->andReturn($getTokenResponse);
 
-        $this->assertTrue($validationErrors->passes());
+        $returnedUserInfo = $this->userDataManager->getUserData('1234');
+
+        $this->assertEquals($expectedResponse, $returnedUserInfo);
+    }
+
+    /**
+     * @test
+     */
+    public function get_user_data_token_passes_curl_call_fails(): void
+    {
+        $getTokenResponse = 'nrtovbe5h02os45krmjzvkt3hp74vf';
+        $getUserDataResponse = [
+            'response' => json_encode([
+                'data' => [
+                    [
+                        'id' => '1234',
+                        'login' => 'zdraste_vladkenov',
+                        'display_name' => 'zdraste_vladkenov',
+                        'type' => '',
+                        'broadcaster_type' => '',
+                        'description' => 'wasde876',
+                        'profile_image_url' => 'https://static-cdn.jtvnw.net/user-default-pictures-uv/ebe4cd89-b4f4-4cd9-adac-2f30151b4209-profile_image-300x300.png',
+                        'offline_image_url' => '',
+                        'view_count' => 0,
+                        'created_at' => '2018-09-04T15:23:04Z'
+                    ]
+                ]
+            ]),
+            'http_code' => 500
+        ];
+        $expectedResponse = ['error' => self::ERROR_GET_USERS_FAILED];
+
+        $this->tokenProvider
+            ->expects('getToken')
+            ->once()
+            ->andReturn($getTokenResponse);
+        $this->apiClient
+            ->expects('makeCurlCall')
+            ->with('https://api.twitch.tv/helix/users?id=1234', [0 => 'Authorization: Bearer nrtovbe5h02os45krmjzvkt3hp74vf'])
+            ->once()
+            ->andReturn($getUserDataResponse);
+
+        $returnedUserInfo = $this->userDataManager->getUserData('1234');
+
+        $this->assertEquals($expectedResponse, $returnedUserInfo);
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+        Mockery::close();
     }
 }
