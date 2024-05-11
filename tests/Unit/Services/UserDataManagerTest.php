6dbd83be6f04c0cec93b7f9073b243f632f71a22
Santi Ayechu <santiayechu@gmail.com>
2024-05-11 13:22:28 +0200

    [29] - Añadir test eliminado sin querer de UserDataManagerTest

---
+++ tests/Unit/Services/UserDataManagerTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\Unit\Services;
+
+use App\Infrastructure\Requests\AnalyticsUsersRequest;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\MessageBag;
+use Tests\TestCase;
+
+class UserDataManagerTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function requestDataIsInvalid(): void
+    {
+        $analyticsUsersRequest = new AnalyticsUsersRequest();
+        $analyticsUsersRequest->merge(['id' => 'abc']);
+        $expectedErrors = new MessageBag([
+            'id' => ['El Id dado no es válido'],
+        ]);
+
+        $validationErrors = Validator::make($analyticsUsersRequest->all(), $analyticsUsersRequest->rules(), $analyticsUsersRequest->messages());
+
+        $this->assertTrue($validationErrors->fails());
+        $this->assertEquals($expectedErrors, $validationErrors->errors());
+    }
+
+    /**
+     * @test
+     */
+    public function idRequiredInRequest(): void
+    {
+        $analyticsUsersRequest = new AnalyticsUsersRequest();
+        $expectedErrors = new MessageBag([
+            'id' => ['El Id es obligatorio'],
+        ]);
+
+        $validationErrors = Validator::make($analyticsUsersRequest->all(), $analyticsUsersRequest->rules(), $analyticsUsersRequest->messages());
+
+        $this->assertTrue($validationErrors->fails());
+        $this->assertEquals($expectedErrors, $validationErrors->errors());
+    }
+
+    /**
+     * @test
+     */
+    public function idIsValid(): void
+    {
+        $analyticsUsersRequest = new AnalyticsUsersRequest();
+        $analyticsUsersRequest->merge(['id' => 1234]);
+
+        $validationErrors = Validator::make($analyticsUsersRequest->all(), $analyticsUsersRequest->rules(), $analyticsUsersRequest->messages());
+
+        $this->assertTrue($validationErrors->passes());
+    }
+}
