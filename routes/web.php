f3f0345e0d8979dfd321c000ae54b44d364508aa
Santi Ayechu <santiayechu@gmail.com>
2024-05-11 19:00:50 +0200

    [28] - Renombrar controlador de GetStreamsController a AnalyticsStreamsController

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Infrastructure\Controllers\AnalyticsUsersController;
-use App\Infrastructure\Controllers\GetStreamsController;
+use App\Infrastructure\Controllers\AnalyticsStreamsController;
 use App\Infrastructure\Controllers\AnalyticsTopsOfTheTopsController;
 
 Route::get('/', function () {
@@ -10,5 +10,5 @@ Route::get('/', function () {
 });
 
 Route::get('/analytics/users', AnalyticsUsersController::class);
-Route::get('/analytics/streams', GetStreamsController::class);
+Route::get('/analytics/streams', AnalyticsStreamsController::class);
 Route::get('/analytics/topsofthetops', AnalyticsTopsOfTheTopsController::class);
