a6ac6422c33100bee21108b433439f9b16332c75
Santi Ayechu <santiayechu@gmail.com>
2024-05-11 13:16:55 +0200

    [29] - Devolver mensajes de error en array y declararlos en constantes

--- app/Services/UserDataManager.php
+++ app/Services/UserDataManager.php
@@ -9,28 +9,31 @@ class UserDataManager
     private TokenProvider $tokenProvider;
     private ApiClient $apiClient;
 
+    private const ERROR_GET_TOKEN_FAILED = 'No se puede establecer conexión con Twitch en este momento';
+    private const ERROR_GET_USERS_FAILED = 'No se pueden devolver usuarios en este momento, inténtalo más tarde';
+
+
     public function __construct(TokenProvider $tokenProvider, ApiClient $apiClient)
     {
         $this->tokenProvider = $tokenProvider;
         $this->apiClient = $apiClient;
     }
 
-    public function getUserData(string $userId): string|array
+    public function getUserData(string $userId): array
     {
         $twitchToken = $this->tokenProvider->getToken();
 
         if ($this->requestHas500Code($twitchToken)) {
-            return '503: {"error": "No se puede establecer conexión con Twitch en este momento}';
+            return ['error' => self::ERROR_GET_TOKEN_FAILED];
         }
 
         $apiUrl = "https://api.twitch.tv/helix/users?id=" . urlencode($userId);
-
         $apiHeaders = ['Authorization: Bearer ' . $twitchToken];
 
         $userData = $this->apiClient->makeCurlCall($apiUrl, $apiHeaders);
 
         if ($this->requestHas500Code($userData)) {
-            return '503: {"error": "No se pueden devolver usuarios en este momento, inténtalo más tarde"}';
+            return ['error' => self::ERROR_GET_USERS_FAILED];
         }
 
         return json_decode($userData['response'], true);
