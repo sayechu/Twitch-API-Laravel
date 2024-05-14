ecea5627960a5ce59ed8a5cb46f59cb55bc117d3
Santi Ayechu <santiayechu@gmail.com>
2024-05-14 09:51:33 +0200

    [0] - Cambiar logica de manejo de error; lanzar excepcion

--- app/Infrastructure/GetStreams/AnalyticsStreamsController.php
+++ app/Infrastructure/GetStreams/AnalyticsStreamsController.php
@@ -2,6 +2,8 @@
 
 namespace App\Infrastructure\GetStreams;
 
+use Exception;
+use Illuminate\Http\Response;
 use App\Infrastructure\Controllers\Controller;
 use App\Services\GetStreamsService;
 use Illuminate\Http\JsonResponse;
@@ -10,7 +12,6 @@ use Illuminate\Http\Request;
 class AnalyticsStreamsController extends Controller
 {
     private GetStreamsService $getStreamsService;
-    private const ERROR_STATUS_CODE = 503;
 
     public function __construct(GetStreamsService $getStreamsService)
     {
@@ -19,16 +20,11 @@ class AnalyticsStreamsController extends Controller
 
     public function __invoke(Request $request): JsonResponse
     {
-        $streamsData = $this->getStreamsService->execute();
-
-        if ($this->containsServerError($streamsData))
-            return response()->json($streamsData, self::ERROR_STATUS_CODE);
-
-        return response()->json($streamsData);
-    }
-
-    private function containsServerError(array $streamsData) : bool
-    {
-        return isset($streamsData['error']);
+        try {
+            $streamsData = $this->getStreamsService->execute();
+            return response()->json($streamsData);
+        } catch (Exception $e) {
+            return response()->json(['error' => $e->getMessage()], Response::HTTP_SERVICE_UNAVAILABLE);
+        }
     }
 }
--- app/Services/StreamsDataManager.php
+++ app/Services/StreamsDataManager.php
@@ -2,14 +2,15 @@
 
 namespace App\Services;
 
+use Exception;
 use Illuminate\Http\Response;
 
 class StreamsDataManager
 {
     private TokenProvider $tokenProvider;
     private ApiClient $apiClient;
-    private const ERROR_GET_TOKEN_FAILED = 'No se puede establecer conexión con Twitch en este momento';
-    private const ERROR_GET_STREAMS_FAILED = 'No se pueden devolver streams en este momento, inténtalo más tarde';
+    private const GET_TOKEN_ERROR_MESSAGE = 'No se puede establecer conexión con Twitch en este momento';
+    private const GET_STREAMS_ERROR_MESSAGE = 'No se pueden devolver streams en este momento, inténtalo más tarde';
     private const STREAMS_URL = 'https://api.twitch.tv/helix/streams';
 
     public function __construct(TokenProvider $tokenProvider, ApiClient $apiClient)
@@ -23,7 +24,7 @@ class StreamsDataManager
         $twitchTokenResponse = $this->tokenProvider->getToken();
 
         if ($this->requestHas500Code($twitchTokenResponse)) {
-            return ['error' => self::ERROR_GET_TOKEN_FAILED];
+            throw new Exception(self::GET_TOKEN_ERROR_MESSAGE);
         }
 
         $apiHeaders = ['Authorization: Bearer ' . $twitchTokenResponse];
@@ -32,7 +33,7 @@ class StreamsDataManager
         $streamsResponse = $this->apiClient->makeCurlCall($apiUrl, $apiHeaders);
 
         if ($this->requestHas500Code($streamsResponse)) {
-            return ['error' => self::ERROR_GET_STREAMS_FAILED];
+            throw new Exception(self::GET_STREAMS_ERROR_MESSAGE);
         }
 
         return json_decode($streamsResponse['response'], true)['data'];
