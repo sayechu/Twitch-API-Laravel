c946d9c5fe82a0f21a6865c81fd1bb8788e5d0f6
Santi Ayechu <santiayechu@gmail.com>
2024-05-14 09:56:23 +0200

    [0] - Renombrar constantes de mensajes de error y añadir Http status code

--- tests/Feature/GetUsersTest.php
+++ tests/Feature/GetUsersTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Http\Response;
 use App\Services\UserDataManager;
 use Illuminate\Foundation\Testing\TestCase;
 use App\Services\ApiClient;
@@ -13,10 +14,11 @@ class GetUsersTest extends TestCase
 {
     private ApiClient $apiClient;
     private DBClient $databaseClient;
-    private const ERROR_GET_TOKEN_FAILED = 'No se puede establecer conexión con Twitch en este momento';
-    private const ERROR_GET_USERS_FAILED = 'No se pueden devolver usuarios en este momento, inténtalo más tarde';
+    private const GET_TOKEN_ERROR_MESSAGE = 'No se puede establecer conexión con Twitch en este momento';
+    private const GET_USERS_ERROR_MESSAGE = 'No se pueden devolver usuarios en este momento, inténtalo más tarde';
     private const TWITCH_TOKEN = 'nrtovbe5h02os45krmjzvkt3hp74vf';
-    private const ERROR_STATUS = 503;
+    private const GET_USERS_URL = 'https://api.twitch.tv/helix/users';
+    private const USER_ID = 1234;
 
     protected function setUp(): void
     {
@@ -59,7 +61,7 @@ class GetUsersTest extends TestCase
                     ]
                 ]
             ]),
-            'http_code' => 200
+            'http_code' => Response::HTTP_OK
         ];
 
         $this->databaseClient
@@ -72,11 +74,11 @@ class GetUsersTest extends TestCase
             ->andReturn(self::TWITCH_TOKEN);
         $this->apiClient
             ->expects('makeCurlCall')
-            ->with("https://api.twitch.tv/helix/users?id=1234", [0 => 'Authorization: Bearer ' . self::TWITCH_TOKEN])
+            ->with(self::GET_USERS_URL . '?id=' . self::USER_ID, [0 => 'Authorization: Bearer ' . self::TWITCH_TOKEN])
             ->once()
             ->andReturn($usersResponse);
 
-        $responseGetUsers = $this->get('/analytics/users?id=1234');
+        $responseGetUsers = $this->get('/analytics/users?id=' . self::USER_ID);
 
         $responseGetUsers->assertContent('{"data":[{"id":"userId","login":"userLogin","display_name":"displayName","type":"type","broadcaster_type":"broadcasterType","description":"description","profile_image_url":"profileImageUrl","offline_image_url":"offlineImageUrl","view_count":0,"created_at":"createdAt"}]}');
     }
@@ -103,7 +105,7 @@ class GetUsersTest extends TestCase
                     ]
                 ]
             ]),
-            'http_code' => 200
+            'http_code' => Response::HTTP_OK
         ];
         $getTokenResponse = [
             'response' => json_encode([
@@ -111,7 +113,7 @@ class GetUsersTest extends TestCase
                 'expires_in' => 5089418,
                 'token_type' => 'bearer'
             ]),
-            'http_code' => 200
+            'http_code' => Response::HTTP_OK
         ];
 
         $this->databaseClient
@@ -128,11 +130,11 @@ class GetUsersTest extends TestCase
             ->with(self::TWITCH_TOKEN);
         $this->apiClient
             ->expects('makeCurlCall')
-            ->with("https://api.twitch.tv/helix/users?id=1234", [0 => 'Authorization: Bearer ' . self::TWITCH_TOKEN])
+            ->with(self::GET_USERS_URL . '?id=' . self::USER_ID, [0 => 'Authorization: Bearer ' . self::TWITCH_TOKEN])
             ->once()
             ->andReturn($userResponse);
 
-        $responseGetUsers = $this->get('/analytics/users?id=1234');
+        $responseGetUsers = $this->get('/analytics/users?id=' . self::USER_ID);
 
         $responseGetUsers->assertContent('{"data":[{"id":"userId","login":"userLogin","display_name":"displayName","type":"type","broadcaster_type":"broadcasterType","description":"description","profile_image_url":"profileImageUrl","offline_image_url":"offlineImageUrl","view_count":0,"created_at":"createdAt"}]}');
     }
@@ -142,10 +144,10 @@ class GetUsersTest extends TestCase
      */
     public function get_users_with_token_from_api_failure()
     {
-        $expectedResponse = json_encode(['error' => self::ERROR_GET_TOKEN_FAILED]);
+        $expectedResponse = json_encode(['error' => self::GET_TOKEN_ERROR_MESSAGE]);
         $getTokenResponse = [
             'response' => null,
-            'http_code' => 500
+            'http_code' => Response::HTTP_INTERNAL_SERVER_ERROR
         ];
 
         $this->databaseClient
@@ -157,9 +159,9 @@ class GetUsersTest extends TestCase
             ->once()
             ->andReturn($getTokenResponse);
 
-        $responseGetUsers = $this->get('/analytics/users?id=1234');
+        $responseGetUsers = $this->get('/analytics/users?id=' . self::USER_ID);
 
-        $responseGetUsers->assertStatus(self::ERROR_STATUS);
+        $responseGetUsers->assertStatus(Response::HTTP_SERVICE_UNAVAILABLE);
         $this->assertEquals($expectedResponse, $responseGetUsers->getContent());
     }
 
@@ -168,10 +170,10 @@ class GetUsersTest extends TestCase
      */
     public function get_users_with_token_stored_returns_users_curl_error()
     {
-        $expectedResponse = json_encode(['error' => self::ERROR_GET_USERS_FAILED]);
+        $expectedResponse = json_encode(['error' => self::GET_USERS_ERROR_MESSAGE]);
         $curlCallResponse = [
             'response' => null,
-            'http_code' => 500
+            'http_code' => Response::HTTP_INTERNAL_SERVER_ERROR
         ];
 
         $this->databaseClient
@@ -185,12 +187,12 @@ class GetUsersTest extends TestCase
         $this->apiClient
             ->expects('makeCurlCall')
             ->once()
-            ->with('https://api.twitch.tv/helix/users?id=1234', [0 => 'Authorization: Bearer ' . self::TWITCH_TOKEN])
+            ->with(self::GET_USERS_URL . '?id=' . self::USER_ID, [0 => 'Authorization: Bearer ' . self::TWITCH_TOKEN])
             ->andReturn($curlCallResponse);
 
-        $responseGetUsers = $this->get('/analytics/users?id=1234');
+        $responseGetUsers = $this->get('/analytics/users?id=' . self::USER_ID);
 
-        $responseGetUsers->assertStatus(self::ERROR_STATUS);
+        $responseGetUsers->assertStatus(Response::HTTP_SERVICE_UNAVAILABLE);
         $this->assertEquals($expectedResponse, $responseGetUsers->getContent());
     }
 
@@ -205,13 +207,13 @@ class GetUsersTest extends TestCase
                 'expires_in' => 5089418,
                 'token_type' => 'bearer'
             ]),
-            'http_code' => 200
+            'http_code' => Response::HTTP_OK
         ];
         $curlCallResponse = [
             'response' => null,
-            'http_code' => 500
+            'http_code' => Response::HTTP_INTERNAL_SERVER_ERROR
         ];
-        $expectedResponse = json_encode(['error' => self::ERROR_GET_USERS_FAILED]);
+        $expectedResponse = json_encode(['error' => self::GET_USERS_ERROR_MESSAGE]);
 
         $this->databaseClient
             ->expects('isTokenStoredInDatabase')
@@ -228,12 +230,12 @@ class GetUsersTest extends TestCase
         $this->apiClient
             ->expects('makeCurlCall')
             ->once()
-            ->with('https://api.twitch.tv/helix/users?id=1234', [0 => 'Authorization: Bearer ' . self::TWITCH_TOKEN])
+            ->with(self::GET_USERS_URL . '?id=' . self::USER_ID, [0 => 'Authorization: Bearer ' . self::TWITCH_TOKEN])
             ->andReturn($curlCallResponse);
 
-        $responseGetUsers = $this->get('/analytics/users?id=1234');
+        $responseGetUsers = $this->get('/analytics/users?id=' . self::USER_ID);
 
-        $responseGetUsers->assertStatus(self::ERROR_STATUS);
+        $responseGetUsers->assertStatus(Response::HTTP_SERVICE_UNAVAILABLE);
         $this->assertEquals($expectedResponse, $responseGetUsers->getContent());
     }
 
