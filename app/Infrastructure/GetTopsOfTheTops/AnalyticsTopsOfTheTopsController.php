90ec3f80355f59f7a609cf7d2af0819a4f3c0d65
Santi Ayechu <santiayechu@gmail.com>
2024-05-14 09:53:17 +0200

    [0] - Agrupar controlador y request en carpeta endpoint TopsOfTheTops

--- app/Infrastructure/GetTopsOfTheTops/AnalyticsTopsOfTheTopsController.php
+++ app/Infrastructure/GetTopsOfTheTops/AnalyticsTopsOfTheTopsController.php
@@ -1,18 +1,20 @@
 <?php
 
-namespace App\Infrastructure\Controllers;
+namespace App\Infrastructure\GetTopsOfTheTops;
 
-use App\Infrastructure\Requests\AnalyticsTopsOfTheTopsRequest;
+use App\Infrastructure\Controllers\Controller;
 use App\Services\GetTopsOfTheTopsService;
 use Illuminate\Http\JsonResponse;
 
 class AnalyticsTopsOfTheTopsController extends Controller
 {
     private GetTopsOfTheTopsService $getTopsOfTopsServ;
+
     public function __construct(GetTopsOfTheTopsService $getTopsOfTopsServ)
     {
         $this->getTopsOfTopsServ = $getTopsOfTopsServ;
     }
+
     public function __invoke(AnalyticsTopsOfTheTopsRequest $request): JsonResponse
     {
         $since = $request->input('since') ?? (10 * 60);
