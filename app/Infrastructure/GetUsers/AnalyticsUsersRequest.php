73170ec31ce9ee6f0a4a98678d58b6d96bd1d8a4
Santi Ayechu <santiayechu@gmail.com>
2024-05-22 10:18:17 +0200

    [0] - Añadir nuevos ficheros en directorios

---
+++ app/Infrastructure/GetUsers/AnalyticsUsersRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Infrastructure\GetUsers;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AnalyticsUsersRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'id' => 'required|numeric',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'id.required' => 'El Id es obligatorio',
+            'id.numeric' => 'El Id dado no es válido',
+        ];
+    }
+}
