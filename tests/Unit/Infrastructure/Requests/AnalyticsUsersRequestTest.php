83f1bdb78c771d43db7f59f6c67ac2b298274d17
Santi Ayechu <santiayechu@gmail.com>
2024-05-22 10:03:32 +0200

    [0] - Solucionar conflictos

--- tests/Unit/Infrastructure/Requests/AnalyticsUsersRequestTest.php
+++ tests/Unit/Infrastructure/Requests/AnalyticsUsersRequestTest.php
@@ -2,25 +2,22 @@
 
 namespace Tests\Unit\Infrastructure\Requests;
 
-use App\Infrastructure\GetStreamers\AnalyticsStreamersRequest;
+use App\Infrastructure\Requests\AnalyticsUsersRequest;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\MessageBag;
 use Tests\TestCase;
 
-class AnalyticsStreamersRequestTest extends TestCase
+class AnalyticsUsersRequestTest extends TestCase
 {
-    private const INVALID_ARGUMENT_MESSAGE = 'El Id dado no es válido';
-    private const REQUIRED_ARGUMENT_MESSAGE = 'El Id es obligatorio';
-
     /**
      * @test
      */
-    public function request_data_is_invalid(): void
+    public function requestDataIsInvalid(): void
     {
-        $analyticsUsersRequest = new AnalyticsStreamersRequest();
+        $analyticsUsersRequest = new AnalyticsUsersRequest();
         $analyticsUsersRequest->merge(['id' => 'abc']);
         $expectedErrors = new MessageBag([
-            'id' => [self::INVALID_ARGUMENT_MESSAGE],
+            'id' => ['El Id dado no es válido'],
         ]);
 
         $validationErrors = Validator::make(
@@ -35,11 +32,11 @@ class AnalyticsStreamersRequestTest extends TestCase
     /**
      * @test
      */
-    public function id_required_in_request(): void
+    public function idRequiredInRequest(): void
     {
-        $analyticsUsersRequest = new AnalyticsStreamersRequest();
+        $analyticsUsersRequest = new AnalyticsUsersRequest();
         $expectedErrors = new MessageBag([
-            'id' => [self::REQUIRED_ARGUMENT_MESSAGE],
+            'id' => ['El Id es obligatorio'],
         ]);
 
         $validationErrors = Validator::make(
@@ -54,9 +51,9 @@ class AnalyticsStreamersRequestTest extends TestCase
     /**
      * @test
      */
-    public function id_is_valid(): void
+    public function idIsValid(): void
     {
-        $analyticsUsersRequest = new AnalyticsStreamersRequest();
+        $analyticsUsersRequest = new AnalyticsUsersRequest();
         $analyticsUsersRequest->merge(['id' => 1234]);
 
         $validationErrors = Validator::make(
