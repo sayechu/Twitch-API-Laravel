10515a1570e5a0fffba2a78bc2b33ad03ec250a2
Santi Ayechu <santiayechu@gmail.com>
2024-05-24 01:13:00 +0200

    [39] - Añadir endpoint de /analytics/users a bootstrap/app para evitar CSRF token

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->validateCsrfTokens(except: [
+            '/analytics/users',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
