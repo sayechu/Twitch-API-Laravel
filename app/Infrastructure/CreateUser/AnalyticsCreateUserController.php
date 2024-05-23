465f3cdc65e8dc7488291b6c6d63a17b9009fb75
eduezponda <eduezponda@gmail.com>
2024-05-23 22:42:26 +0200

    [39] - Crear controlador

--- app/Infrastructure/CreateUser/AnalyticsCreateUserController.php
+++ app/Infrastructure/CreateUser/AnalyticsCreateUserController.php
@@ -1,10 +1,39 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Infrastructure\CreateUser;
 
+use App\Exceptions\ConflictException;
+use App\Exceptions\InternalServerErrorException;
+use App\Infrastructure\Controllers\Controller;
+use App\Services\CreateUserManager;
+use Exception;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
-class AnalyticsCreateUserController
+class AnalyticsCreateUserController extends Controller
 {
-    //
+    private CreateUserManager $createUserManager;
+
+    public function __construct(CreateUserManager $createUserManager)
+    {
+        $this->createUserManager = $createUserManager;
+    }
+    public function __invoke(Request $request): JsonResponse
+    {
+        $username = $request->input('username');
+        $password = $request->input('password');
+
+        if (empty($username) || empty($password)) {
+            return response()->json(['error' => 'Los parámetros (username y password) no fueron proporcionados.'], 400);
+        }
+
+        try {
+            $createUserMessage = $this->createUserManager->getCreateUserMessage($username, $password);
+            return response()->json($createUserMessage, 201);
+        } catch (ConflictException $e) {
+            return response()->json(['error' => $e->getMessage()], 409);
+        } catch (InternalServerErrorException $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
 }
