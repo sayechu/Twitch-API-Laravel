ee1ababb8da984f5933fde28be4eeb66a6347f13
eduezponda <eduezponda@gmail.com>
2024-05-23 18:12:37 +0200

    [0] - Refactorizar tests y servicios con streamers

--- tests/Feature/GetStreamersTest.php
+++ tests/Feature/GetStreamersTest.php
@@ -9,20 +9,17 @@ use App\Services\ApiClient;
 use App\Services\DBClient;
 use App\Services\TokenProvider;
 use Mockery;
-use Tests\Builders\StreamerDTO;
-use Tests\Builders\StreamerDTOBuilder;
 
 class GetStreamersTest extends TestCase
 {
     private ApiClient $apiClient;
     private DBClient $databaseClient;
-    private StreamerDTO $expectedStreamerDTO;
     private const GET_TOKEN_ERROR_MESSAGE = 'No se puede establecer conexión con Twitch en este momento';
-    private const GET_STREAMERS_ERROR_MESSAGE = 'No se pueden devolver usuarios en este momento, inténtalo más tarde';
+    private const GET_USERS_ERROR_MESSAGE = 'No se pueden devolver usuarios en este momento, inténtalo más tarde';
     private const TWITCH_TOKEN = 'nrtovbe5h02os45krmjzvkt3hp74vf';
-    private const GET_STREAMERS_URL = 'https://api.twitch.tv/helix/users';
+    private const GET_USERS_URL = 'https://api.twitch.tv/helix/users';
     private const ENDPOINT = "streamers";
-    private const STREAMER_ID = 1234;
+    private const USER_ID = 1234;
 
     protected function setUp(): void
     {
@@ -41,28 +38,29 @@ class GetStreamersTest extends TestCase
             ->when(StreamersDataManager::class)
             ->needs(ApiClient::class)
             ->give(fn() => $this->apiClient);
-        $this->expectedStreamerDTO = (new StreamerDTOBuilder())
-            ->withId('userId')
-            ->withLogin('userLogin')
-            ->withDisplayName('displayName')
-            ->withType('type')
-            ->withBroadcasterType('broadcasterType')
-            ->withDescription('description')
-            ->withProfileImageUrl('profileImageUrl')
-            ->withOfflineImageUrl('offlineImageUrl')
-            ->withViewCount(0)
-            ->withCreatedAt('2024-05-08T07:35:07Z')
-            ->build();
     }
 
     /**
      * @test
      */
-    public function get_streamers_with_token_stored_returns_user_data(): void
+    public function get_users_with_token_stored_returns_user_data(): void
     {
-        $streamersResponse = [
+        $usersResponse = [
             'response' => json_encode([
-                'data' => [$this->expectedStreamerDTO->toArray()]
+                'data' => [
+                    [
+                        'id' => 'userId',
+                        'login' => 'userLogin',
+                        'display_name' => 'displayName',
+                        'type' => 'type',
+                        'broadcaster_type' => 'broadcasterType',
+                        'description' => 'description',
+                        'profile_image_url' => 'profileImageUrl',
+                        'offline_image_url' => 'offlineImageUrl',
+                        'view_count' => 0,
+                        'created_at' => 'createdAt'
+                    ]
+                ]
             ]),
             'http_code' => Response::HTTP_OK
         ];
@@ -77,23 +75,36 @@ class GetStreamersTest extends TestCase
             ->andReturn(self::TWITCH_TOKEN);
         $this->apiClient
             ->expects('makeCurlCall')
-            ->with(self::GET_STREAMERS_URL . '?id=' . self::STREAMER_ID, [0 => 'Authorization: Bearer ' . self::TWITCH_TOKEN])
+            ->with(self::GET_USERS_URL . '?id=' . self::USER_ID, [0 => 'Authorization: Bearer ' . self::TWITCH_TOKEN])
             ->once()
-            ->andReturn($streamersResponse);
+            ->andReturn($usersResponse);
 
-        $responseGetStreamers = $this->get('/analytics/' . self::ENDPOINT . '?id=' . self::STREAMER_ID);
+        $responseGetUsers = $this->get('/analytics/' . self::ENDPOINT . '?id=' . self::USER_ID);
 
-        $responseGetStreamers->assertContent('{"data":[{"id":"userId","login":"userLogin","display_name":"displayName","type":"type","broadcaster_type":"broadcasterType","description":"description","profile_image_url":"profileImageUrl","offline_image_url":"offlineImageUrl","view_count":0,"created_at":"2024-05-08T07:35:07Z"}]}');
+        $responseGetUsers->assertContent('{"data":[{"id":"userId","login":"userLogin","display_name":"displayName","type":"type","broadcaster_type":"broadcasterType","description":"description","profile_image_url":"profileImageUrl","offline_image_url":"offlineImageUrl","view_count":0,"created_at":"createdAt"}]}');
     }
 
     /**
      * @test
      */
-    public function get_streamers_with_token_from_api_returns_user_data()
+    public function get_users_with_token_from_api_returns_user_data()
     {
-        $streamersResponse = [
+        $userResponse = [
             'response' => json_encode([
-                'data' => [$this->expectedStreamerDTO->toArray()]
+                'data' => [
+                    [
+                        'id' => 'userId',
+                        'login' => 'userLogin',
+                        'display_name' => 'displayName',
+                        'type' => 'type',
+                        'broadcaster_type' => 'broadcasterType',
+                        'description' => 'description',
+                        'profile_image_url' => 'profileImageUrl',
+                        'offline_image_url' => 'offlineImageUrl',
+                        'view_count' => 0,
+                        'created_at' => 'createdAt'
+                    ]
+                ]
             ]),
             'http_code' => Response::HTTP_OK
         ];
@@ -120,19 +131,19 @@ class GetStreamersTest extends TestCase
             ->with(self::TWITCH_TOKEN);
         $this->apiClient
             ->expects('makeCurlCall')
-            ->with(self::GET_STREAMERS_URL . '?id=' . self::STREAMER_ID, [0 => 'Authorization: Bearer ' . self::TWITCH_TOKEN])
+            ->with(self::GET_USERS_URL . '?id=' . self::USER_ID, [0 => 'Authorization: Bearer ' . self::TWITCH_TOKEN])
             ->once()
-            ->andReturn($streamersResponse);
+            ->andReturn($userResponse);
 
-        $responseGetStreamers = $this->get('/analytics/' . self::ENDPOINT . '?id=' . self::STREAMER_ID);
+        $responseGetUsers = $this->get('/analytics/' . self::ENDPOINT . '?id=' . self::USER_ID);
 
-        $responseGetStreamers->assertContent('{"data":[{"id":"userId","login":"userLogin","display_name":"displayName","type":"type","broadcaster_type":"broadcasterType","description":"description","profile_image_url":"profileImageUrl","offline_image_url":"offlineImageUrl","view_count":0,"created_at":"2024-05-08T07:35:07Z"}]}');
+        $responseGetUsers->assertContent('{"data":[{"id":"userId","login":"userLogin","display_name":"displayName","type":"type","broadcaster_type":"broadcasterType","description":"description","profile_image_url":"profileImageUrl","offline_image_url":"offlineImageUrl","view_count":0,"created_at":"createdAt"}]}');
     }
 
     /**
      * @test
      */
-    public function get_streamers_with_token_from_api_failure()
+    public function get_users_with_token_from_api_failure()
     {
         $expectedResponse = json_encode(['error' => self::GET_TOKEN_ERROR_MESSAGE]);
         $getTokenResponse = [
@@ -149,18 +160,18 @@ class GetStreamersTest extends TestCase
             ->once()
             ->andReturn($getTokenResponse);
 
-        $responseGetStreamers = $this->get('/analytics/' . self::ENDPOINT . '?id=' . self::STREAMER_ID);
+        $responseGetUsers = $this->get('/analytics/' . self::ENDPOINT . '?id=' . self::USER_ID);
 
-        $responseGetStreamers->assertStatus(Response::HTTP_SERVICE_UNAVAILABLE);
-        $this->assertEquals($expectedResponse, $responseGetStreamers->getContent());
+        $responseGetUsers->assertStatus(Response::HTTP_SERVICE_UNAVAILABLE);
+        $this->assertEquals($expectedResponse, $responseGetUsers->getContent());
     }
 
     /**
      * @test
      */
-    public function get_streamers_with_token_stored_returns_users_curl_error()
+    public function get_users_with_token_stored_returns_users_curl_error()
     {
-        $expectedResponse = json_encode(['error' => self::GET_STREAMERS_ERROR_MESSAGE]);
+        $expectedResponse = json_encode(['error' => self::GET_USERS_ERROR_MESSAGE]);
         $curlCallResponse = [
             'response' => null,
             'http_code' => Response::HTTP_INTERNAL_SERVER_ERROR
@@ -177,19 +188,19 @@ class GetStreamersTest extends TestCase
         $this->apiClient
             ->expects('makeCurlCall')
             ->once()
-            ->with(self::GET_STREAMERS_URL . '?id=' . self::STREAMER_ID, [0 => 'Authorization: Bearer ' . self::TWITCH_TOKEN])
+            ->with(self::GET_USERS_URL . '?id=' . self::USER_ID, [0 => 'Authorization: Bearer ' . self::TWITCH_TOKEN])
             ->andReturn($curlCallResponse);
 
-        $responseGetStreamers = $this->get('/analytics/' . self::ENDPOINT . '?id=' . self::STREAMER_ID);
+        $responseGetUsers = $this->get('/analytics/' . self::ENDPOINT . '?id=' . self::USER_ID);
 
-        $responseGetStreamers->assertStatus(Response::HTTP_SERVICE_UNAVAILABLE);
-        $this->assertEquals($expectedResponse, $responseGetStreamers->getContent());
+        $responseGetUsers->assertStatus(Response::HTTP_SERVICE_UNAVAILABLE);
+        $this->assertEquals($expectedResponse, $responseGetUsers->getContent());
     }
 
     /**
      * @test
      */
-    public function get_streamers_with_token_from_api_returns_users_curl_error()
+    public function get_users_with_token_from_api_returns_users_curl_error()
     {
         $getTokenResponse = [
             'response' => json_encode([
@@ -203,7 +214,7 @@ class GetStreamersTest extends TestCase
             'response' => null,
             'http_code' => Response::HTTP_INTERNAL_SERVER_ERROR
         ];
-        $expectedResponse = json_encode(['error' => self::GET_STREAMERS_ERROR_MESSAGE]);
+        $expectedResponse = json_encode(['error' => self::GET_USERS_ERROR_MESSAGE]);
 
         $this->databaseClient
             ->expects('isTokenStoredInDatabase')
@@ -220,13 +231,13 @@ class GetStreamersTest extends TestCase
         $this->apiClient
             ->expects('makeCurlCall')
             ->once()
-            ->with(self::GET_STREAMERS_URL . '?id=' . self::STREAMER_ID, [0 => 'Authorization: Bearer ' . self::TWITCH_TOKEN])
+            ->with(self::GET_USERS_URL . '?id=' . self::USER_ID, [0 => 'Authorization: Bearer ' . self::TWITCH_TOKEN])
             ->andReturn($curlCallResponse);
 
-        $responseGetStreamers = $this->get('/analytics/' . self::ENDPOINT . '?id=' . self::STREAMER_ID);
+        $responseGetUsers = $this->get('/analytics/' . self::ENDPOINT . '?id=' . self::USER_ID);
 
-        $responseGetStreamers->assertStatus(Response::HTTP_SERVICE_UNAVAILABLE);
-        $this->assertEquals($expectedResponse, $responseGetStreamers->getContent());
+        $responseGetUsers->assertStatus(Response::HTTP_SERVICE_UNAVAILABLE);
+        $this->assertEquals($expectedResponse, $responseGetUsers->getContent());
     }
 
     protected function tearDown(): void
