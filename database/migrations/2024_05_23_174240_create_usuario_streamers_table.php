8fa1686b28ec001f28a60601e177af69d02462d9
Santi Ayechu <santiayechu@gmail.com>
2024-06-01 13:29:26 +0200

    [0] - Solucionar conflictos migraciones

--- database/migrations/2024_05_23_174240_create_usuario_streamers_table.php
+++ database/migrations/2024_05_23_174240_create_usuario_streamers_table.php
@@ -1,14 +1,9 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         $sql = "
@@ -22,10 +17,7 @@ return new class extends Migration
 
         DB::statement($sql);
     }
-
-    /**
-     * Reverse the migrations.
-     */
+    
     public function down(): void
     {
         DB::statement("DROP TABLE IF EXISTS USUARIO_STREAMERS");
