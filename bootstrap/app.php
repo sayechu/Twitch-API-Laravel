77f6b931d2206805b973978566e5ab5d7c373201
Santi Ayechu <santiayechu@gmail.com>
2024-06-01 13:32:23 +0200

    [42] - Solucionar conflicto al añadir endpoint unfollow para quitar CSRF token

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->validateCsrfTokens(except: [
-            'analytics/follow'
+            'analytics/follow',
+            '/analytics/users',
+            '/analytics/unfollow'
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
