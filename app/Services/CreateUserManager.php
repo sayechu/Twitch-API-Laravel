05b353a12658b80a0a39a4de982e16b950c8c37a
eduezponda <133292010+eduezponda@users.noreply.github.com>
2024-06-02 18:39:23 +0200

    [36] - Implementar endpoint Follow streamer (#44)
    * [36] - Arreglar conflictos
    
    * [36] - Arreglar conflictos DBClient
    
    * [0] - Refactorizar servicios con streamers
    
    * [36] - Arreglar conflictos migraciones usuario
    
    * [36] - Crear excepciones forbidden y unauthorized
    
    * [36] - Implementado error get token
    
    * [36] - Devolver mensajes excepcionesd desde controller
    
    * [36] - Añadir follow a routes
    
    * [36] - Modificar streamerId a string en usuario_streamers
    
    * [36] - Añadir excepcion para manejar error not found
    
    * [36] - Manejar errores desde controlador
    
    * [36] - Eliminar metodo innecesario post en ApiClient
    
    * [36] - Refactorizar DBClient
    
    * [36] - Refactorizar follow streamer manager
    
    * [36] - Implementar request con username y streamerId
    
    * [36] - Implementar metodos database y devolver errores en excepciones
    
    * [36] - Manejar mensajes excepciones desde fuera del controller
    
    * [36] - Refactorizar FollowStreamerManager
    
    * [36] - Implementar tests de integracion
    
    * [36] - Implementar tests unitarios
    
    * [36] - Eliminar endpoint desactualizado de /users en /follow
    
    ---------
    
    Co-authored-by: Santi Ayechu <santiayechu@gmail.com>

--- app/Services/CreateUserManager.php
+++ app/Services/CreateUserManager.php
@@ -8,8 +8,6 @@ use App\Exceptions\InternalServerErrorException;
 class CreateUserManager
 {
     private DBClient $dbClient;
-    private const CREATE_USER_MESSAGE = 'Usuario creado correctamente';
-    private const CONFLICT_ERROR_MESSAGE = 'El nombre de usuario ya está en uso.';
 
     public function __construct(DBClient $dbClient)
     {
@@ -25,14 +23,14 @@ class CreateUserManager
         $userExists = $this->dbClient->checkIfUsernameExists($username);
 
         if ($userExists) {
-            throw new ConflictException(self::CONFLICT_ERROR_MESSAGE);
+            throw new ConflictException("El nombre de usuario ya está en uso.");
         }
 
         $this->dbClient->createUser($username, $password);
 
         return [
             "username" => $username,
-            "message" => self::CREATE_USER_MESSAGE
+            "message" => "Usuario creado correctamente"
         ];
     }
 }
