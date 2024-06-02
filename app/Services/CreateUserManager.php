f33dadf8bb8d63b9ecd529c08ccb51182f3500dd
eduezponda <eduezponda@gmail.com>
2024-06-02 19:24:34 +0200

    [39] - Refactorizar constantes mensajes

--- app/Services/CreateUserManager.php
+++ app/Services/CreateUserManager.php
@@ -8,6 +8,8 @@ use App\Exceptions\InternalServerErrorException;
 class CreateUserManager
 {
     private DBClient $dbClient;
+    private const CREATE_USER_MESSAGE = 'Usuario creado correctamente';
+    private const CONFLICT_ERROR_MESSAGE = 'El nombre de usuario ya está en uso.';
 
     public function __construct(DBClient $dbClient)
     {
@@ -23,14 +25,14 @@ class CreateUserManager
         $userExists = $this->dbClient->checkIfUsernameExists($username);
 
         if ($userExists) {
-            throw new ConflictException("El nombre de usuario ya está en uso.");
+            throw new ConflictException(self::CONFLICT_ERROR_MESSAGE);
         }
 
         $this->dbClient->createUser($username, $password);
 
         return [
             "username" => $username,
-            "message" => "Usuario creado correctamente"
+            "message" => self::CREATE_USER_MESSAGE
         ];
     }
 }
