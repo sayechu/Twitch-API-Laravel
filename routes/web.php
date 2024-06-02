0809c3954321baa2662fd34d0e3c2612dac33361
Santi Ayechu <santiayechu@gmail.com>
2024-06-02 17:11:50 +0200

    [37] - Añadir endpoint a routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Infrastructure\FollowStreamer\AnalyticsFollowStreamerController;
 use App\Infrastructure\GetStreams\AnalyticsStreamsController;
 use App\Infrastructure\GetTopsOfTheTops\AnalyticsTopsOfTheTopsController;
 use App\Infrastructure\GetStreamers\AnalyticsStreamersController;
+use App\Infrastructure\Timeline\AnalyticsTimelineController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,3 +17,4 @@ Route::get('/analytics/streams', AnalyticsStreamsController::class);
 Route::get('/analytics/topsofthetops', AnalyticsTopsOfTheTopsController::class);
 Route::get('/analytics/follow', AnalyticsFollowStreamerController::class);
 Route::post('/analytics/users', AnalyticsCreateUserController::class);
+Route::get('/analytics/timeline', AnalyticsTimelineController::class);
