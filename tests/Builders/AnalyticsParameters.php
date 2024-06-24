c3d94836199c1c29ca06e3cce506c813e7c33bda
Santi Ayechu <santi.ayechu@540deg.com>
2024-06-24 21:08:41 +0200

    [0] - Eliminar tipo de constante de AnalyticsParameters.php

--- tests/Builders/AnalyticsParameters.php
+++ tests/Builders/AnalyticsParameters.php
@@ -11,7 +11,7 @@ class AnalyticsParameters
     public const ANALYTICS_GET_STREAMS_URL = 'https://api.twitch.tv/helix/streams';
     public const TWITCH_TOKEN = 'nrtovbe5h02os45krmjzvkt3hp74vf';
     public const STREAMER_ID = '1234';
-    public const string STREAMERS_ENDPOINT = '/analytics/streamers';
+     public const STREAMERS_ENDPOINT = '/analytics/streamers';
     public const TOP_GAMES_URL= 'https://api.twitch.tv/helix/games/top?first=3';
     public const SINCE_TIME = 600;
     public const GAME_ID = 509658;
